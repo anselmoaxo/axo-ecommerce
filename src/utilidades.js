091a3aeb0cc3edab74a4cedd84801489ee9433b5
Anselmo <anselmo.cstecnologia@gmail.com>
2023-10-18 00:00:36 -0300

    projeto incial ecommerce javascrit

---
+++ src/utilidades.js
@@ -0,0 +1,46 @@
+export const catalogo = [
+  {
+    id: "1",
+    nome: "Cofre Preto",
+    marca: "Eletronicos AXO",
+    preco: 250,
+    nomeArquivoImagem: "prod01.jpg",
+  },
+  {
+    id: "2",
+    nome: "teste 1",
+    marca: "Eletronicos AXO",
+    preco: 250,
+    nomeArquivoImagem: "prod02.jpg",
+  },
+
+  {
+    id: "3",
+    nome: "teste 2",
+    marca: "Eletronicos AXO",
+    preco: 250,
+    nomeArquivoImagem: "prod03.jpg",
+  },
+
+  {
+    id: "4",
+    nome: "teste 3",
+    marca: "Eletronicos AXO",
+    preco: 250,
+    nomeArquivoImagem: "prod04.jpg",
+  },
+  {
+    id: " 5",
+    nome: "teste 4",
+    marca: "Eletronicos AXO",
+    preco: 250,
+    nomeArquivoImagem: "prod05.jpg",
+  },
+  {
+    id: "6",
+    nome: "teste 5",
+    marca: "Eletronicos AXO",
+    preco: 250,
+    nomeArquivoImagem: "prod06.jpg",
+  },
+];
